bf4b8d9b00b86a173c139640b70c6659f5ae3e08
Patrick Andrade <patrick.oliveiraandrade@epfl.ch>
2013-10-14 23:04:07 +0200

    Upload the main code of the TelnetServer example

---
+++ TelnetExample/src/epfl/concurency/bachelorProject/TelnetServer.java
@@ -0,0 +1,81 @@
+package epfl.concurency.bachelorProject;
+
+/*
+ *	Author:      Patrick Oliveira Andrade
+ *	Date:        14 oct. 2013
+ */
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TelnetServer {
+	private final static int PORT = 11211;
+	
+	public static void main(String[] args) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			
+			System.out.println("Telnet server on port " + PORT + " started sucessfully !");
+
+			while (true) {
+
+				Socket socket = serverSocket.accept();
+
+				OutputStream os = socket.getOutputStream();
+				DataOutputStream dout = new DataOutputStream(os);
+
+				dout.writeChars("Welcome to the Telnet channel " + PORT + " ! ");
+				dout.writeChars("Please start communicating:");
+
+				new Thread(new Connection(socket)).start();
+
+			}
+		} catch (IOException e) {
+			System.out.println("Impossible to start the Telnet server !");
+		}
+
+	}
+}
+
+class Connection implements Runnable {
+	private Socket mSocket;
+	private BufferedReader bfr;
+	private DataOutputStream dout;
+	
+	private final static String QUIT_COMMAND = "quit";
+
+	public Connection(Socket socket) throws IOException {
+		mSocket = socket;
+		bfr = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+		dout = new DataOutputStream(mSocket.getOutputStream());
+		System.out.println("	Started connection with " + mSocket.getInetAddress());
+	}
+
+	public void run() {
+		String command = "";
+		
+		while(!command.equals(QUIT_COMMAND)){
+			try {
+				command = bfr.readLine();
+				
+				System.out.println("I read the command: " + command);
+				dout.writeChars("Thanks for requesting the command " + command + "\n");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			mSocket.close();
+			System.err.println("	-> Connection with " + mSocket.getInetAddress() + " aborted!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
