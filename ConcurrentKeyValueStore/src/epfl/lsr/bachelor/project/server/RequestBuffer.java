2f695f02f9d2348286e05cee78d74b3647a87cdc
GregoryMaitre <gregory.maitre@epfl.ch>
2013-10-16 15:01:46 +0200

    Add a buffer of request

---
+++ ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/server/RequestBuffer.java
@@ -0,0 +1,15 @@
+package epfl.lsr.bachelor.project.server;
+
+import java.util.ArrayList;
+
+import epfl.lsr.bachelor.project.server.request.Request;
+
+/**
+ * TODO: Comment this class
+ * 
+ * @author Gregory Maitre & Patrick Andrade
+ * 
+ */
+public class RequestBuffer {
+	private ArrayList<Request> requestList = new ArrayList<>();
+}
