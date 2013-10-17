3983e11d8899a733ad15c3be26725534428859fe
PatrickAndrade <patrick.oliveiraandrade@epfl.ch>
2013-10-17 21:23:34 +0200

    Add a point of entry to the application

---
+++ ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/starter/Starter.java
@@ -0,0 +1,18 @@
+package epfl.lsr.bachelor.project.starter;
+
+import epfl.lsr.bachelor.project.server.Server;
+
+/**
+ * Point of entry of the application
+ * 
+ * @author Gregory Maitre & Patrick Andrade
+ * 
+ */
+public final class Starter {
+
+	public static void main(String[] args) {
+		Server.start();
+	}
+
+}
+
