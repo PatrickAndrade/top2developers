e4dafcbb7e3742be92c0353f7a6a5f2d1959d32d
PatrickAndrade <patrick.oliveiraandrade@epfl.ch>
2013-12-03 10:41:17 +0100

    Create ReadRequest

---
+++ ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/server/request/ReadRequest.java
@@ -0,0 +1,16 @@
+package epfl.lsr.bachelor.project.server.request;
+
+/**
+ * Represent a readble request
+ * 
+ * @author Gregory Maitre & Patrick Andrade
+ * 
+ */
+public abstract class ReadRequest extends Request {
+
+    public ReadRequest(String key) {
+        super(key);
+    }
+
+}
+
