57a69eb8cc5c6be1aa16f4ce70ab0d4cef0254ce
PatrickAndrade <patrick.oliveiraandrade@epfl.ch>
2013-12-04 15:04:16 +0100

    Doc of ValueInteger

--- ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/values/ValueInteger.java
+++ ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/values/ValueInteger.java
@@ -3,54 +3,59 @@ package epfl.lsr.bachelor.project.values;
 import epfl.lsr.bachelor.project.util.Utilities;
 
 /**
- * Integer Value
+ * This is an an integer value contained in the KV-store
  * 
  * @author Gregory Maitre & Patrick Andrade
  * 
  */
 public class ValueInteger extends Value<Integer> {
 
-	public ValueInteger(int value) {
-		super(value);
-	}
+    /**
+     * Constructor of the integer value
+     * 
+     * @param value
+     *            the value to be represented
+     */
+    public ValueInteger(int value) {
+        super(value);
+    }
 
-	@Override
-	public boolean supportIncrementDecrement() {
-		return true;
-	}
+    @Override
+    public boolean supportIncrementDecrement() {
+        return true;
+    }
 
-	@Override
-	// This will increment the value by k
-	public void increment(int k) {
-		if ((double) getValue() / (double) Integer.MAX_VALUE + (double) k
-				/ (double) Integer.MAX_VALUE <= 1
-				&& (double) getValue() / (double) Integer.MIN_VALUE
-						+ (double) k / (double) Integer.MIN_VALUE <= 1) {
-			setValue(getValue() + k);
-		}
-	}
+    @Override
+    // This will increment the value by k
+    public void increment(int k) {
+        if ((double) getValue() / (double) Integer.MAX_VALUE + (double) k / (double) Integer.MAX_VALUE <= 1
+                && (double) getValue() / (double) Integer.MIN_VALUE + (double) k / (double) Integer.MIN_VALUE <= 1) {
+            setValue(getValue() + k);
+        }
+    }
 
-	@Override
-	// This will decrement the value by k, or in others words,
-	// increment by -k
-	public void decrement(int k) {
-		increment(-k);
-	}
-	
-	@Override
-	public String toString() {
-		return getValue().toString();
-	}
+    @Override
+    // This will decrement the value by k, or in others words,
+    // increment by -k
+    public void decrement(int k) {
+        increment(-k);
+    }
 
-	public Value<?> append(Value<?> value) {
-		String newValueString = String.valueOf(getValue()) + String.valueOf(value.getValue());
-		Value<?> newValue = null;
-		if (!Utilities.isInteger(newValueString)) {
-			newValue = new ValueString(newValueString);
-		} else {
-			setValue(Integer.valueOf(newValueString));
-		}
-		
-		return newValue;
-	}
+    @Override
+    public Value<?> append(Value<?> value) {
+        String newValueString = String.valueOf(getValue()) + String.valueOf(value.getValue());
+        Value<?> newValue = null;
+        if (!Utilities.isInteger(newValueString)) {
+            newValue = new ValueString(newValueString);
+        } else {
+            setValue(Integer.valueOf(newValueString));
+        }
+
+        return newValue;
+    }
+
+    @Override
+    public String toString() {
+        return getValue().toString();
+    }
 }
