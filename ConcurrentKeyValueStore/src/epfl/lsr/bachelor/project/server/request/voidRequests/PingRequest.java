acc86b20a518eec9d99f1519eb8a00da7162aecc
PatrickAndrade <patrick.oliveiraandrade@epfl.ch>
2013-12-04 17:00:29 +0100

    Review of PingRequest

--- ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/server/request/voidRequests/PingRequest.java
+++ ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/server/request/voidRequests/PingRequest.java
@@ -3,7 +3,7 @@ package epfl.lsr.bachelor.project.server.request.voidRequests;
 import epfl.lsr.bachelor.project.server.request.VoidRequest;
 
 /**
- * Represent a get request received from the client
+ * Represent a ping request
  * 
  * @author Gregory Maitre & Patrick Andrade
  * 
