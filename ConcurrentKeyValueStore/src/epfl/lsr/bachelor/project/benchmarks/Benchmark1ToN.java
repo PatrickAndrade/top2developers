ec648676ce3b553358a1348615837d86705858be
GregoryMaitre <gregory.maitre@epfl.ch>
2015-04-25 20:19:27 +0200

    Improve test bench

--- ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/benchmarks/Benchmark1ToN.java
+++ ConcurrentKeyValueStore/src/epfl/lsr/bachelor/project/benchmarks/Benchmark1ToN.java
@@ -1,27 +1,67 @@
 package epfl.lsr.bachelor.project.benchmarks;
 
-import java.io.DataOutputStream;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
 
 public class Benchmark1ToN {
 	
 	static String[] request = {"set", "get"};
-	final static int maxNumberOfClients = 10;
+	final static int maxNumberOfClients = 100;
 	final static int numberRequestSend = 100;
 	
 	public static void main(String[] args) throws IOException {
-		DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(new File("results.txt")));
+		System.out.println("Test begin");
+		File file = new File("results.txt");
+		
+		if (file.exists()) {
+			file.delete();
+		}
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		BarreDeProgression barreDeProgression = new BarreDeProgression(maxNumberOfClients);
 		double average = 0;
+		
 		for (int numberOfClients = 1; numberOfClients < maxNumberOfClients; numberOfClients++) {
 			GeneralBenchmarkPipelined generalBenchmarkPipelined = new GeneralBenchmarkPipelined(
 					request, numberRequestSend, numberOfClients);
 			average = generalBenchmarkPipelined.start();
-			dataOutputStream.writeUTF(Double.toString(average) + "\n");
+			writer.write(Double.toString(average) + "\n");
+			barreDeProgression.increment();
 		}
 		
-		dataOutputStream.close();
+		System.out.println("Test end");
+		writer.close();
+	}
+}
+
+class BarreDeProgression extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JProgressBar barreProgression;
+	private int i = 0;
+
+	public BarreDeProgression(int max) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Barre de progression");
+		setLayout(new FlowLayout());
+		setPreferredSize(new Dimension(175, 70));
+		setResizable(false);
+		barreProgression = new JProgressBar(0, max - 1);
+		barreProgression.setStringPainted(true);
+		getContentPane().add(barreProgression);
+		pack();
+		setVisible(true);
+	}
+
+	public void increment() {
+		i++;
+		barreProgression.setValue(i);
 	}
 }
