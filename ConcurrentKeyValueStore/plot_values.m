6f4772e14bbdf194c6caa68d78b177226e257912
PatrickAndrade <patrick.oliveiraandrade@epfl.ch>
2015-04-29 23:09:11 +0200

    Improve powerpoint by adding first results

--- ConcurrentKeyValueStore/plot_values.m
+++ ConcurrentKeyValueStore/plot_values.m
@@ -1,18 +1,45 @@
 % This script is used to plot the data against each other.
-load resultsIOSingle_1toN.txt;
-load resultsIOMultiPipelinedKeysLock_1toN.txt;
-
-x=1:1:100; %range from 1 to 100 with step 1
-y=resultsIOSingle;
-z=resultsIOMultiPipelinedKeysLock;
+x=1:1:500; %range from 1 to 100 with step 1
 
 figure;
 hold all;
-plot(x,y);
-plot(x,z);
+
+load Results\StarterIOMultiGlobalLock.txt;
+load Results\StarterIOMultiKeysLock.txt;
+plot(x,StarterIOMultiGlobalLock);
+plot(x,StarterIOMultiKeysLock);
+
+load Results\StarterIOMultiMapsLock.txt;
+load Results\StarterIOMultiPipelinedGlobalLock.txt;
+plot(x,StarterIOMultiMapsLock);
+plot(x,StarterIOMultiPipelinedGlobalLock);
+
+load Results\StarterIOMultiPipelinedKeysLock.txt;
+load Results\StarterIOMultiPipelinedMapsLock.txt;
+plot(x,StarterIOMultiPipelinedKeysLock);
+plot(x,StarterIOMultiPipelinedMapsLock);
+
+load Results\StarterIOSingle.txt;
+load Results\StarterIOSinglePipelined.txt;
+plot(x,StarterIOSingle);
+plot(x,StarterIOSinglePipelined);
+
+load Results\StarterNIOMultiGlobalLock.txt;
+load Results\StarterNIOMultiKeysLock.txt;
+plot(x,StarterNIOMultiGlobalLock);
+plot(x,StarterNIOMultiKeysLock);
+
+load Results\StarterNIOMultiMapsLock.txt;
+load Results\StarterNIOSingle.txt;
+plot(x,StarterNIOMultiMapsLock);
+plot(x,StarterNIOSingle);
 
 xlabel('Clients');
-ylabel('Average serving time (microseconds)');
-legend('Single threaded IO Server','Multi threaded IO Server with keys lock')
+ylabel('Average waiting time (microseconds)');
+legend('IOMultiGlobalLock','IOMultiKeysLock','IOMultiMapsLock','IOMultiPipelinedGlobalLock','IOMultiPipelinedKeysLock','IOMultiPipelinedMapsLock','IOSingle','IOSinglePipelined','NIOMultiGlobalLock','NIOMultiKeysLock','NIOMultiMapsLock','NIOSingle')
 grid on;
 zoom on;
+
+%print -r1600 -depsc2 part3_lambda70_withoutTransient
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 16 12])
+print('1toN','-dpng','-r0')
